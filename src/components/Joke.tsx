c08b06afdb4e4bcd95d0ee587f95016f40611c49
bajerovak <130050980+bajerovak@users.noreply.github.com>
2026-05-19 13:22:50 +0200

    Update Joke.tsx

--- src/components/Joke.tsx
+++ src/components/Joke.tsx
@@ -1,1 +1,17 @@
+import type { Joke } from "../type";
 
+interface JokeProps {
+  joke: Joke;
+}
+
+
+export default function Joke({ joke }: JokeProps) {
+  return (
+    <div>
+      <h2>{joke.setup}</h2>
+      <p>{joke.punchline}</p>
+      <p>Category: {joke.category}</p>
+      <p>Rating: {joke.rating}</p>
+    </div>
+  );
+}
